5a5b7f1020486e4078e461de87dfbbe68158c3f1
roxanavp87 <roxanavp87@gmail.com>
2017-06-19 15:34:23 -0500

    Create a page at /roll-dice that asks the user to guess a number

---
+++ src/main/java/com/codeup/controller/RollDice.java
@@ -0,0 +1,30 @@
+package com.codeup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by roxana on 6/19/17.
+ */
+@Controller
+public class RollDice {
+    @GetMapping("/rolldice")
+    public String RollDiceC() {
+        return "rolldice";
+    }
+
+    @PostMapping("/rolldice")
+    public String create(@RequestParam(name = "guessedNumber") String number, Model model){
+        int random = (int) (Math.round(Math.random()*5) + 1);
+        if(Integer.parseInt(number) == random) {
+            model.addAttribute("youGess", true);
+        } else {
+            model.addAttribute("youGess", false);
+        }
+        return "rolldice_view";
+    }
+}
---
+++ src/main/java/com/codeup/models/Post.java
@@ -0,0 +1,7 @@
+package com.codeup.models;
+
+/**
+ * Created by roxana on 6/19/17.
+ */
+public class Post {
+}

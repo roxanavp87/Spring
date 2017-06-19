9dc5ae679ffc9fc6158663e52bdc6730f0ab44ec
roxanavp87 <roxanavp87@gmail.com>
2017-06-19 11:44:42 -0500

    Created PostsController class

--- src/main/java/com/codeup/controller/PostsController.java
+++ src/main/java/com/codeup/controller/PostsController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -20,5 +21,17 @@ public class PostsController {
     public String ViewAPosts(@PathVariable int id, Model model) {
         model.addAttribute("id", id);
         return "posts";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String ShowPostForm() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String savePost() {
+        return  "create a new post";
     }
 }

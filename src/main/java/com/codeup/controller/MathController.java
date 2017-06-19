7c02d4bbad69be0e3145cd5e7ce4aa2e3826eaaa
roxanavp87 <roxanavp87@gmail.com>
2017-06-19 10:18:34 -0500

    - spring-blog

---
+++ src/main/java/com/codeup/controller/MathController.java
@@ -0,0 +1,18 @@
+package com.codeup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by roxana on 6/19/17.
+ */
+@Controller
+public class MathController {
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
+    }
+}

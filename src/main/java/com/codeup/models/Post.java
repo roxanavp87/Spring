d237d7ef12198412ec210157bdebcf72b650badd
roxanavp87 <roxanavp87@gmail.com>
2017-06-19 15:35:49 -0500

    Create Post class

--- src/main/java/com/codeup/models/Post.java
+++ src/main/java/com/codeup/models/Post.java
@@ -4,4 +4,27 @@ package com.codeup.models;
  * Created by roxana on 6/19/17.
  */
 public class Post {
+    private String title;
+    private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
